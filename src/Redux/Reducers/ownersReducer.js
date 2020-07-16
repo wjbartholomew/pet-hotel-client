d3963773eef122ceb6a9a2ac10765f7807d12786
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-07-16 13:52:12 -0500

    CONVERT ARRAY INTO OBJECT

--- src/Redux/Reducers/ownersReducer.js
+++ src/Redux/Reducers/ownersReducer.js
@@ -1,10 +1,20 @@
 const owners = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_OWNERS':
-            return action.payload;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "SET_OWNERS":
+      console.log(action.payload.data);
+      let petOwners = action.payload.data;
+
+      const ownerObjects = petOwners.map(([key, firstName, lastName]) => ({
+        key,
+        firstName,
+        lastName,
+      }));
+
+      console.log(ownerObjects);
+      return ownerObjects;
+    default:
+      return state;
+  }
 };
 
 // componentsReducer will be on the redux state at:
