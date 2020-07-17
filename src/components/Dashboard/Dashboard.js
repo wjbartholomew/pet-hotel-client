f726c69c4fc107a13b05ca459a389e2aad0072d9
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-07-17 12:41:11 -0500

    Returning td keeps the user static in table

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -111,13 +111,11 @@ class Dashboard extends Component {
               {this.props.reduxState.pets.map((item) => {
                 return (
                   <tr>
-                    <td>
-                      {this.props.reduxState.owners.map((owner) => {
-                        if (item.owners_id === owner.key) {
-                          return owner.firstName;
-                        }
-                      })}
-                    </td>
+                    {this.props.reduxState.owners.map((owner) => {
+                      if (item.owners_id === owner.key) {
+                        return <td>{owner.firstName}</td>;
+                      }
+                    })}
                     <td>{item.name}</td>
                     <td>{item.breed}</td>
                     <td>{item.color}</td>
